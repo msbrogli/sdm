b7f88413b3bd447436ddbe15da5a16971aadc3a0
msbrogli <msbrogli@gmail.com>
2011-01-12 14:32:19 -0200

    [default] bs_bitset and bs_bitclear tests

--- python/tests.py
+++ python/tests.py
@@ -21,6 +21,32 @@ class BitstringTest(unittest.TestCase):
             dist = sum([ 1 for x, y in zip(str(a), str(b)) if x != y ])
             self.assertEqual(Bitstring.distance(a, b), dist)
 
+    def test_bitset(self):
+        dim = sdm.get_dimension()
+        a = Bitstring(zero=True)
+        for i in range(dim):
+            a.bitset(i)
+
+            s = str(a)
+            s1 = sum([ 1 for x in s[:dim-i-1] if x == '0' ])
+            s2 = sum([ 1 for x in s[-i-1:] if x == '1' ])
+            self.assertEqual(s1+s2, dim)
+
+    def test_bitclear(self):
+        dim = sdm.get_dimension()
+        a = Bitstring(zero=True)
+        for i in range(dim):
+            a.bitset(i)
+        self.assertEqual(sum([ 1 for x in str(a) if x == '1' ]), dim)
+
+        for i in range(dim):
+            a.bitclear(i)
+
+            s = str(a)
+            s1 = sum([ 1 for x in s[:dim-i-1] if x == '1' ])
+            s2 = sum([ 1 for x in s[-i-1:] if x == '0' ])
+            self.assertEqual(s1+s2, dim)
+
 
 if __name__ == '__main__':
     unittest.main()
