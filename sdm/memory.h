a7ae69c00427025845f7faa4471f26e33de2b207
msbrogli <none@none>
2011-01-14 18:25:18 -0200

    [default] moving sdm_find_near_params struct from code to header

--- sdm/memory.h
+++ sdm/memory.h
@@ -7,6 +7,12 @@
 #include "bitstring.h"
 #include "hardlocation.h"
 
+typedef struct {
+	int id;
+	bitstring* address;
+	unsigned int counter;
+} sdm_find_near_params;
+
 // TODO comment
 int sdm_initialize();
 void sdm_free();
